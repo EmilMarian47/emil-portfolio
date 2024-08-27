8d9431638662e56c7d6138ce1e262741d8863aa7
whoismidlaj <its.me.midlaj@gmail.com>
2024-08-27 15:33:37 +0530

    1234

--- pages/disclosures.vue
+++ pages/disclosures.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container py-16">
-        <h1 class="font-sans text-2xl leading-8 mb-6">Privacy Policy</h1>
+        <h1 class="font-sans text-2xl leading-8 mb-6">Important disclosures</h1>
   
         <div class="mb-10 flex flex-col gap-6">
             <p>Lorem ipsum dolor sit amet consectetur. Amet justo et ut sed viverra in at. In amet lectus semper rhoncus. Duis pretium dui commodo non eu et ac dolor proin. Blandit nunc id nascetur lacus dolor.</p>
