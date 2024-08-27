49558b9e0fb9a3df1862b078ce246f7920bb3df4
whoismidlaj <its.me.midlaj@gmail.com>
2024-08-27 15:29:19 +0530

    Added legal pages

--- components/Header.vue
+++ components/Header.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="container pt-12">
         <div class="flex gap-4 items-center mb-7">
-            <img src="../assets/img/logo.png" alt="Logo" class="w-16 h-16">
-            <h1 class="font-sans text-3xl">Emil Marian</h1>
+            <nuxt-link to="/"><img src="../assets/img/logo.png" alt="Logo" class="w-16 h-16"></nuxt-link>
+            <nuxt-link to="/"><h1 class="font-sans text-3xl">Emil Marian</h1></nuxt-link>
         </div>
         <div class="flex justify-between gap-2 py-2 border-y border-[#BCBCBC]">
             <nuxt-link class="font-medium" :class="{'text-[#00000080]': $route.path !== '/', 'text-[#A000AE]': $route.path === '/'}" to="/">About</nuxt-link>
---
+++ pages/privacy.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container py-16">
+        <h1 class="font-sans text-2xl leading-8 mb-6">Privacy Policy</h1>
+  
+        <div class="mb-10 flex flex-col gap-6">
+            <p>Lorem ipsum dolor sit amet consectetur. Amet justo et ut sed viverra in at. In amet lectus semper rhoncus. Duis pretium dui commodo non eu et ac dolor proin. Blandit nunc id nascetur lacus dolor.</p>
+            <p>Fermentum ut iaculis convallis etiam ultrices lorem. Ac dictum pulvinar faucibus vulputate mattis interdum nunc. Ultrices tincidunt est malesuada mi mauris. Orci mattis pellentesque laoreet in magna tristique. Ac sagittis id cras convallis erat ultrices gravida viverra. Tincidunt leo nunc aliquam etiam sed viverra sed.</p>
+            <p>Fermentum ut iaculis convallis etiam ultrices lorem. Ac dictum pulvinar faucibus vulputate mattis interdum nunc. Ultrices tincidunt est malesuada mi mauris. Orci mattis pellentesque laoreet in magna tristique.</p>
+        </div>
+    </div>
+  </template>
+  
