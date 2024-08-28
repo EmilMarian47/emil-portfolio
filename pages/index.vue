382540af53db50c3f7441e53e8e7be14ed3c6cf1
EmilMarian47 <brotherhood47emil@gmail.com>
2024-08-28 16:20:48 +0530

    Update index.vue Contents

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="container py-16">
-        <h1 class="font-sans text-2xl leading-8 mb-6">UI/UX Designer with 3 years of industry experience</h1>
+        <h1 class="font-sans text-2xl leading-8 mb-6">Hello there! 👋</h1>
 
         <div class="font-mono text-base mb-10">
             <p>
-                Lorem ipsum dolor sit amet consectetur. Amet justo et ut sed viverra in at. In amet lectus semper
-                rhoncus. Duis pretium dui commodo non eu et ac dolor proin. Blandit nunc id nascetur lacus dolor.
+                I'm a UI/UX Designer with 2.8 years of industry experience. Lorem ipsum dolor sit amet consectetur. 
+                Amet justo et ut sed viverra in at. In amet lectus semper rhoncus. Duis pretium dui commodo 
+                non eu et ac dolor proin. Blandit nunc id nascetur lacus dolor.
             </p>
             <p>
                 Fermentum ut iaculis convallis etiam ultrices lorem. Ac dictum pulvinar faucibus vulputate mattis
