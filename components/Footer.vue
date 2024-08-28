bc05775849c3acfdfcd2a55dc9feaa2d096a0621
EmilMarian47 <brotherhood47emil@gmail.com>
2024-08-28 11:53:53 +0000

    footer underline

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="container">
         <div class="flex gap-4 flex-wrap justify-center md:justify-between text-xs font-mono border-t border-[#BCBCBC] pt-[10px] pb-8">
-            <nuxt-link class="hover:underline"to="/disclosures">Important disclosures</nuxt-link>
-            <nuxt-link class="hover:underline"to="/terms">Website terms of use</nuxt-link>
-            <nuxt-link class="hover:underline"to="/privacy">Privacy policy</nuxt-link>
+            <nuxt-link class="underline"to="/disclosures">Important disclosures</nuxt-link>
+            <nuxt-link class="underline"to="/terms">Website terms of use</nuxt-link>
+            <nuxt-link class="underline"to="/privacy">Privacy policy</nuxt-link>
         </div>
     </div>
 </template>
