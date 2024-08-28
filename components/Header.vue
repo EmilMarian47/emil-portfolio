ba13162a4822522249a9e4a63208bd899219a241
EmilMarian47 <brotherhood47emil@gmail.com>
2024-08-28 17:19:25 +0530

    Update Header.vue underline revert

--- components/Header.vue
+++ components/Header.vue
@@ -5,9 +5,9 @@
             <nuxt-link to="/"><h1 class="font-sans text-3xl">Emil Marian</h1></nuxt-link>
         </div>
         <div class="flex justify-between gap-2 py-2 border-y border-[#BCBCBC]">
-            <nuxt-link class="font-medium underline underline-offset-2" :class="{'text-[#00000080]': $route.path !== '/', 'text-[#A000AE]': $route.path === '/'}" to="/">About</nuxt-link>
-            <nuxt-link class="font-medium underline underline-offset-2" :class="{'text-[#00000080]': $route.path !== '/works', 'text-[#A000AE]': $route.path === '/works'}" to="/works">Works</nuxt-link>
-            <nuxt-link class="font-medium underline underline-offset-2" :class="{'text-[#00000080]': $route.path !== '/contact', 'text-[#A000AE]': $route.path === '/contact'}" to="/contact">Contact</nuxt-link>
+            <nuxt-link class="font-medium underline" :class="{'text-[#00000080]': $route.path !== '/', 'text-[#A000AE]': $route.path === '/'}" to="/">About</nuxt-link>
+            <nuxt-link class="font-medium underline" :class="{'text-[#00000080]': $route.path !== '/works', 'text-[#A000AE]': $route.path === '/works'}" to="/works">Works</nuxt-link>
+            <nuxt-link class="font-medium underline" :class="{'text-[#00000080]': $route.path !== '/contact', 'text-[#A000AE]': $route.path === '/contact'}" to="/contact">Contact</nuxt-link>
         </div>
     </div>
 </template>
