b1ba6cbace2bbf6becd3fa692cfba8e6a98f3f39
EmilMarian47 <brotherhood47emil@gmail.com>
2024-09-01 04:03:30 +0000

    privacy pages

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,7 @@
-<template>
+<template> 
     <div class="container">
-        <div class="flex gap-4 flex-wrap justify-between text-base font-mono border-t border-gray border-dashed pt-[10px] pb-8">
-            <nuxt-link class="underline" :class="{'text-tertiary': $route.path !== '/disclosures', 'text-secondary': $route.path === '/disclosures'}" to="/disclosures">Important disclosures</nuxt-link>
-            <nuxt-link class="underline" :class="{'text-tertiary': $route.path !== '/terms', 'text-secondary': $route.path === '/terms'}" to="/terms">Website terms of use</nuxt-link>
-            <nuxt-link class="underline" :class="{'text-tertiary': $route.path !== '/privacy', 'text-secondary': $route.path === '/privacy'}" to="/privacy">Privacy policy</nuxt-link>
+        <div class="flex items-center justify-center text-base font-mono border-t border-gray border-dashed pt-[10px] pb-8">
+            <nuxt-link class="underline" :class="{'text-tertiary': $route.path !== '/disclosures', 'text-secondary': $route.path === '/disclosures'}" to="/disclosures">Copyright © 2022-2024 Emil Marian. </nuxt-link>
         </div>
     </div>
 </template>
--- pages/disclosures.vue
+++ pages/disclosures.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container py-8">
-        <h1 class="font-sans text-base leading-8 mb-6">Important disclosures</h1>
+        <h1 class="font-sans text-base leading-8 mb-6">Copyright Protected</h1>
   
         <div class="mb-10 flex flex-col gap-6">
             <p>Lorem ipsum dolor sit amet consectetur. Amet justo et ut sed viverra in at. In amet lectus semper rhoncus. Duis pretium dui commodo non eu et ac dolor proin. Blandit nunc id nascetur lacus dolor.</p>
