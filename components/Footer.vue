da52841074a5263a85b1bf36e6fd4328f2e9865a
EmilMarian47 <brotherhood47emil@gmail.com>
2026-08-24 14:12:06 +0530

    Update Footer.vue reduced font size

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,9 @@
 <template> 
     <div class="container">
-        <div class="flex items-center justify-center text-base font-mono border-t border-gray border-dashed pt-[10px] pb-8">
-            <nuxt-link class="underline" :class="{'text-tertiary': $route.path !== '/disclosures', 'text-secondary': $route.path === '/disclosures'}" to="/disclosures">Copyright © 2022-2024 Emil Marian. </nuxt-link>
+        <div class="flex items-center justify-center text-xs font-sans border-t border-gray border-dashed pt-[10px] pb-8">
+            <nuxt-link class="underline" :class="{'text-tertiary': $route.path !== '/disclosures', 'text-secondary': $route.path === '/disclosures'}" to="/disclosures">
+                Copyright © 2026-2028 Emil Marian.
+            </nuxt-link>
         </div>
     </div>
 </template>
